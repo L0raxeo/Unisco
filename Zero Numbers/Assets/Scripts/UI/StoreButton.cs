a713df203af55e3d38b06c2553975fb3321f7fcc
L0raxeo <67399230+L0raxeo@users.noreply.github.com>
2021-04-10 11:10:48 -0400

    Store Button
    - Added store button functionality
    - Can enter store and exit store

---
+++ Zero Numbers/Assets/Scripts/UI/StoreButton.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class StoreButton : MonoBehaviour
+{
+
+    public GameObject storeUI;
+
+    public void OnClick()
+    {
+        GameObject.Find("Shade").GetComponent<Animator>().SetBool("isSolid", true);
+        storeUI.SetActive(true);
+    }
+
+}
